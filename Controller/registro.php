4607737332d9dd7a3480718de09c2ad958c70eb9
vallma25 <vallmajorjoana@gmail.com>
2025-05-02 15:04:31 +0200

    millora de registrament registro.php

--- Controller/registro.php
+++ Controller/registro.php
@@ -1,7 +1,8 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-include 'DataBase.php';
+include_once '../Model/DataBase.php';
+
 
 $bd = new ConexionBD();
 $conexion = $bd->getConexion();
