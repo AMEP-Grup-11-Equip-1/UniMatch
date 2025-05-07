c7c225c2b60bf40caae03efa3c946b4db9a56fde
lariCFL <larissafpires@gmail.com>
2025-05-07 23:27:34 +0200

    borrar todo

--- error_handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_SESSION['error'])) {
-    $error_message = $_SESSION['error'];
-    unset($_SESSION['error']); // Limpa a mensagem após exibi-la
-} else {
-    $error_message = "";
-}
-?>
